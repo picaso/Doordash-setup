ab3a470fab7148c3785642faae4bcddecdc61a83
Osaide <picaso@users.noreply.github.com>
2021-09-10 12:59:03 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 import unittest
 
-
+# Can multiply two numbers
+# Can divide two numbers
 
 class CalculatorTest(unittest.TestCase):
     def test_should_add_two_numbes(self):
@@ -11,6 +12,12 @@ class CalculatorTest(unittest.TestCase):
         calculator = Calculator()
         self.assertEqual(calculator.add(3,2,6), 11, "output should be 11")
 
+def test_should_find_difference_between_two_numbers(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.difference(2,2), 0)
+
+
+
 class Calculator():
     def add(self, *args):
         sum = 0 
@@ -18,8 +25,5 @@ class Calculator():
             sum = sum + arg
         return sum
 
-
-
-
-# import functools        
-# return functools.reduce(lambda a, b: a+b, args)
+    def difference(self, a, b):
+        return a - b
